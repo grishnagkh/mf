d70768abc34b58e7279ca1c0ccd975aa3c826914
stefan petscharnig <stepetsc@edu.uni-klu.ac.at>
2014-07-28 11:33:38 +0200

    added synchronization, so that concurrently executed requesthandlers do not interfere

--- sync/fine/FSResponseHandler.java
+++ sync/fine/FSResponseHandler.java
@@ -30,7 +30,7 @@ public class FSResponseHandler extends Thread {
 	private String msg;
 	public static final String TAG = "FSResponse Handler";
 	private FSync parent;
-	
+
 	private int maxId;
 	private int myId;
 
@@ -42,83 +42,74 @@ public class FSResponseHandler extends Thread {
 	}
 
 	public void run() {
-		BloomFilter<Integer> bloom = parent.getBloom();
-		if (bloom == null)
-			return;
 
 		String[] msgA = msg.split("\\" + SyncI.DELIM);
 
 		BloomFilter<Integer> rcvBF = new BloomFilter<Integer>(
 				SyncI.BITS_PER_ELEM, SyncI.N_EXP_ELEM, SyncI.N_HASHES);
 
-		rcvBF.setBitSet(Utils.fromString(msgA[4]));
-
-		int paketMax = Integer.parseInt(msgA[5]);
-
-		int nBloom1 = Utils.getN(rcvBF, paketMax);
-		int nBloom2 = Utils.getN(bloom, maxId);
-
-		Log.d(TAG, "#peers in received bf: " + nBloom1 + ", maxId: " + paketMax);
-		Log.d(TAG, "#peers in stored bf: " + nBloom2 + ", maxId: " + maxId);
-
 		long rAvg = Long.parseLong(msgA[1]);
 		long rNtp = Long.parseLong(msgA[2]);
 
-		long actTs = Utils.getTimestamp();
-		
-		long avgTs = parent.alignAvgTs(actTs);
+		rcvBF.setBitSet(Utils.fromString(msgA[4]));
+		int paketMax = Integer.parseInt(msgA[5]);
 
-		if (Utils.xor(rcvBF, bloom)) {// the bloom filters are different
-			Log.d(TAG, "bloom filters are different");
-			if (!Utils.and(rcvBF, bloom)) {// the bloom filters do not
-											// overlap
-				Log.d(TAG, "bloom filters do not overlap");
-				// merge the filters
-				bloom.getBitSet().or(rcvBF.getBitSet());
+		synchronized (parent) {
 
-				// calc weighted average
+			BloomFilter<Integer> bloom = parent.getBloom();
+			if (bloom == null)
+				return;
+			int nBloom1 = Utils.getN(rcvBF, paketMax);
+			int nBloom2 = Utils.getN(bloom, maxId);
 
-				long wSum = (avgTs * nBloom2 + (rAvg + (actTs - rNtp))
-						* nBloom1);
+			Log.d(TAG, "#peers in received bf: " + nBloom1 + ", maxId: "
+					+ paketMax);
+			Log.d(TAG, "#peers in stored bf: " + nBloom2 + ", maxId: " + maxId);
 
-				parent.updateAvgTs(wSum / (nBloom1 + nBloom2));
+			long actTs = Utils.getTimestamp();
+			long avgTs = parent.alignAvgTs(actTs);
 
-				// add the received bloom filter to the ones already seen
-				parent.getBloomList().add(rcvBF);
+			/*
+			 * TODO: does the list "contain" the bf when the same are sent over
+			 * the network? to test..,. if not, a comparison method must be
+			 * written
+			 */
 
-			} else if (!parent.getBloomList().contains(rcvBF)
-					&& nBloom1 < nBloom2) {
-				Log.d(TAG, "bloom filters do overlap, we have not seen the "
-						+ "bloom filter before and the received "
-						+ "bloom filter has more information");
-				/*
-				 * TODO: does the list "contain" the bf when the same are sent
-				 * over the network? to test..,. if not, a comparison method
-				 * must be written
-				 */
-				// overlap and received bloom filter has more information
-				bloom = rcvBF;
-				if (bloom.contains(myId)) {
-					Log.d(TAG, "we already are in this bloom filter, "
-							+ "just take this average");
-					// take the received average and correct it
-					parent.updateAvgTs(rAvg + (actTs - rNtp));
-				} else {
-					Log.d(TAG, "we are not in this bloom filter, update "
-							+ "average and add "
-							+ "ourself to the bloom filter");
-					// take the received time stamp and add our own
-					long wSum = (nBloom1 * (rAvg + (actTs - rNtp)) + avgTs);
-					parent.updateAvgTs(wSum / (nBloom2 + 1));
-					bloom.add(myId);
+			if (Utils.xor(rcvBF, bloom)) {// the bloom filters are different
+				Log.d(TAG, "bloom filters are different");
+				if (!Utils.and(rcvBF, bloom)) {// the bloom filters do not
+												// overlap
+					Log.d(TAG, "bloom filters do not overlap");
+					/* merge the filters */
+					bloom.getBitSet().or(rcvBF.getBitSet());
+					long wSum = (avgTs * nBloom2 + (rAvg + (actTs - rNtp))
+							* nBloom1);
+					parent.updateAvgTs(wSum / (nBloom1 + nBloom2));
+					/* add the received bloom filter to the ones already seen */
+					parent.getBloomList().add(rcvBF);
+
+				} else if (!parent.getBloomList().contains(rcvBF)
+						&& nBloom1 < nBloom2) {
+
+					/* overlap and received bloom filter has more information */
+					bloom = rcvBF;
+					if (bloom.contains(myId)) {
+						/* correct received average */
+						parent.updateAvgTs(rAvg + (actTs - rNtp));
+					} else {
+						/* add own timestamp to received one  */
+						long wSum = (nBloom1 * (rAvg + (actTs - rNtp)) + avgTs);
+						parent.updateAvgTs(wSum / (nBloom2 + 1));
+						bloom.add(myId);
+					}
 				}
+
+			} else {
+				// the same bloom filters: ignore; time stamps must be equal
 			}
-		} else {
-			// the same bloom filters: ignore; time stamps must be equal
 		}
 
-		maxId = maxId < paketMax ? paketMax : maxId;
-		parent.setMaxId(maxId);
+		parent.setMaxId(maxId < paketMax ? paketMax : maxId);
 
 	}
 }
