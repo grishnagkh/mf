128bbe9281ae7e2629b285fd7f3308367c6f15a0
stefan petscharnig <stepetsc@edu.uni-klu.ac.at>
2014-08-11 15:33:43 +0200

    added license to syncmsg

--- sync/net/SyncMsg.java
+++ sync/net/SyncMsg.java
@@ -1,3 +1,24 @@
+/*
+ * SyncMsg.java
+ *
+ * Copyright (c) 2014, Stefan Petscharnig. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
 package mf.sync.net;
 
 import java.io.Serializable;
@@ -6,7 +27,7 @@ import java.net.InetAddress;
 public abstract class SyncMsg implements Serializable {
 
 	private static final long serialVersionUID = 7179713119327407162L;
-	
+
 	public int peerId;
 	public int msgId;
 	public int port;
